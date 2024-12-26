673650109c2ccf630d2df8e2ff6a37e783dd18ba
HaMyoungHoon <basicouts@gmail.com>
2024-12-26 17:10:02 +0900

    route 추가

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,13 @@ export const routes: Routes = [
   { path: "", component: AppMainComponent,
     children: [
       { path: FConstants.DASH_BOARD_URL, loadChildren: () => import("./components/app-main/dash-board/dash-board.module").then(m => m.DashBoardModule) },
+
+      { path: FConstants.EDI_REQUEST, loadChildren: () => import("./components/app-main/edi/edi-request/edi-request.module").then(m => m.EdiRequestModule) },
+      { path: FConstants.EDI_LIST, loadChildren: () => import("./components/app-main/edi/edi-list/edi-list.module").then(m => m.EdiListModule) },
+
+      { path: FConstants.MY_INFO_URL, loadChildren: () => import("./components/app-main/etc/my-info/my-info.module").then(m => m.MyInfoModule) },
+      { path: FConstants.MEDICINE_PRICE_LIST_URL, loadChildren: () => import("./components/app-main/etc/medicine-price-list/medicine-price-list.module").then(m => m.MedicinePriceListModule) },
+      { path: FConstants.QNA_LIST_URL, loadChildren: () => import("./components/app-main/etc/qna/qna-list.module").then(m => m.QnaListModule) },
     ]
   },
   { path: FConstants.NOTFOUND_URL.slice(1), loadChildren: () => import("./components/not-found/not-found.module").then(m => m.NotFoundModule) },
