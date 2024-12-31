cafd327b524a63b572c86ea36c0c456c37bff561
HaMyoungHoon <basicouts@gmail.com>
2024-12-31 11:49:37 +0900

    const string 변경

--- src/app/models/common/menu-items/menu-item-2.ts
+++ src/app/models/common/menu-items/menu-item-2.ts
@@ -10,13 +10,13 @@ export function MenuItem2(): MenuItem {
         label: "menu-2.sub-menu.sub-1",
         icon: "pi pi-fw pi-cloud-upload",
         styleClass: "top-menu",
-        routerLink: `/${FConstants.EDI_REQUEST}`,
+        routerLink: `/${FConstants.EDI_REQUEST_URL}`,
       },
       {
         label: "menu-2.sub-menu.sub-2",
         icon: "pi pi-fw pi-list-check",
         styleClass: "top-menu",
-        routerLink: `/${FConstants.EDI_LIST}`,
+        routerLink: `/${FConstants.EDI_LIST_URL}`,
       },
     ]
   };
