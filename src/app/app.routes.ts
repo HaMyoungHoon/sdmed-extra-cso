b4882263cef3d15de525c67603e902d1f79c498a
HaMyoungHoon <basicouts@gmail.com>
2024-12-31 11:49:34 +0900

    const string 변경

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,8 +7,8 @@ export const routes: Routes = [
     children: [
       { path: FConstants.DASH_BOARD_URL, loadChildren: () => import("./components/app-main/dash-board/dash-board.module").then(m => m.DashBoardModule) },
 
-      { path: FConstants.EDI_REQUEST, loadChildren: () => import("./components/app-main/edi/edi-request/edi-request.module").then(m => m.EdiRequestModule) },
-      { path: FConstants.EDI_LIST, loadChildren: () => import("./components/app-main/edi/edi-list/edi-list.module").then(m => m.EdiListModule) },
+      { path: FConstants.EDI_REQUEST_URL, loadChildren: () => import("./components/app-main/edi/edi-request/edi-request.module").then(m => m.EdiRequestModule) },
+      { path: FConstants.EDI_LIST_URL, loadChildren: () => import("./components/app-main/edi/edi-list/edi-list.module").then(m => m.EdiListModule) },
 
       { path: FConstants.MY_INFO_URL, loadChildren: () => import("./components/app-main/etc/my-info/my-info.module").then(m => m.MyInfoModule) },
       { path: FConstants.MEDICINE_PRICE_LIST_URL, loadChildren: () => import("./components/app-main/etc/medicine-price-list/medicine-price-list.module").then(m => m.MedicinePriceListModule) },
--- src/app/guards/f-constants.ts
+++ src/app/guards/f-constants.ts
@@ -1,31 +1,31 @@
-export const AUTH_TOKEN = "token";
+export const AUTH_TOKEN: string = "token";
 
-export const STORAGE_KEY_LANG = "lang";
-export const THEME_LINK = "app-theme";
+export const STORAGE_KEY_LANG: string = "lang";
+export const THEME_LINK: string = "app-theme";
 
-export const NOTFOUND_URL = "/notfound";
-export const API_CSO = "/apiCSO";
+export const NOTFOUND_URL: string = "/notfound";
+export const API_CSO: string = "/apiCSO";
 
-export const DASH_BOARD_URL = "dashBoard";
+export const DASH_BOARD_URL: string = "dashBoard";
 
-export const EDI_REQUEST = "ediRequest";
-export const EDI_LIST = "ediList";
+export const EDI_REQUEST_URL: string = "ediRequest";
+export const EDI_LIST_URL: string = "ediList";
 
-export const MY_INFO_URL = "myInfo";
-export const MEDICINE_PRICE_LIST_URL = "medicine-price-list";
-export const QNA_LIST_URL = "qnaList";
-export const QNA_WRITE_URL = "qnaWrite";
-export const QNA_VIEW_URL = "qnaList/:thisPK";
+export const MY_INFO_URL: string = "myInfo";
+export const MEDICINE_PRICE_LIST_URL: string = "medicine-price-list";
+export const QNA_LIST_URL: string = "qnaList";
+export const QNA_WRITE_URL: string = "qnaWrite";
+export const QNA_VIEW_URL: string = "qnaList/:thisPK";
 
-export const ASSETS_NO_IMAGE = "assets/image/no-image-1920.png";
-export const ASSETS_ZIP_IMAGE = "assets/image/zip-image.png";
-export const ASSETS_PDF_IMAGE = "assets/image/pdf-image.png";
-export const ASSETS_XLSX_IMAGE = "assets/image/excel-image.png";
-export const ASSETS_DOCX_IMAGE = "assets/image/word-image.png";
+export const ASSETS_NO_IMAGE: string = "assets/image/no-image-1920.png";
+export const ASSETS_ZIP_IMAGE: string = "assets/image/zip-image.png";
+export const ASSETS_PDF_IMAGE: string = "assets/image/pdf-image.png";
+export const ASSETS_XLSX_IMAGE: string = "assets/image/excel-image.png";
+export const ASSETS_DOCX_IMAGE: string = "assets/image/word-image.png";
 
-export const BLOB_URL = "https://mhhablob1.blob.core.windows.net";
-export const BLOB_CONTAINER_NAME = "mhhablob1";
+export const BLOB_URL: string = "https://mhhablob1.blob.core.windows.net";
+export const BLOB_CONTAINER_NAME: string = "mhhablob1";
 
 export const tableStyle: {"min-width": string} = {"min-width": "20rem"};
-export const filterTableOption = "contains";
+export const filterTableOption: string = "contains";
 export const galleriaContainerStyle: {"width": string, "height": string} = {"width": "300px", "height": "360px"};
