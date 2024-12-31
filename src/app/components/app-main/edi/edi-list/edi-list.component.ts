2464317c550550cf7b10293c761a6d97d146ff0f
HaMyoungHoon <basicouts@gmail.com>
2024-12-31 13:32:53 +0900

    edi 목록 추가

--- src/app/components/app-main/edi/edi-list/edi-list.component.ts
+++ src/app/components/app-main/edi/edi-list/edi-list.component.ts
@@ -1,6 +1,13 @@
-import {Component} from "@angular/core";
+import {Component, ViewChild} from "@angular/core";
 import {FComponentBase} from "../../../../guards/f-component-base";
 import {UserRole} from "../../../../models/rest/user/user-role";
+import * as FExtensions from "../../../../guards/f-extensions";
+import * as FConstants from "../../../../guards/f-constants";
+import {EDIUploadModel} from "../../../../models/rest/edi/edi-upload-model";
+import {getEdiStateSeverity} from "../../../../guards/f-extensions";
+import {EDIStateToEDIStateDesc} from "../../../../models/rest/edi/edi-state";
+import {EdiListService} from "../../../../services/rest/edi-list.service";
+import {Calendar} from "primeng/calendar";
 
 @Component({
   selector: "app-edi-list",
@@ -9,7 +16,86 @@ import {UserRole} from "../../../../models/rest/user/user-role";
   standalone: false,
 })
 export class EdiListComponent extends FComponentBase{
-  constructor() {
+  @ViewChild("startCalendar") startCalendar !: Calendar;
+  @ViewChild("endCalendar") endCalendar !: Calendar;
+  startDate: Date = FExtensions.plusMonths(new Date(), -1);
+  endDate: Date = new Date();
+  isSorted: boolean | null = null;
+  initValue: EDIUploadModel[] = [];
+  viewList: EDIUploadModel[] = [];
+  constructor(private thisService: EdiListService) {
     super(Array<UserRole>(UserRole.Admin, UserRole.CsoAdmin, UserRole.BusinessMan));
   }
+
+  override async ngInit(): Promise<void> {
+    await this.getList();
+  }
+
+  async getList(): Promise<void> {
+    const ret = await FExtensions.restTry(async() => await this.thisService.getList(FExtensions.dateToYYYYMMdd(this.startDate), FExtensions.dateToYYYYMMdd(this.endDate)),
+      e => this.fDialogService.error("getList", e));
+    if (ret.result) {
+      this.initValue = ret.data ?? [];
+      this.viewList = [...this.initValue];
+      return;
+    }
+    this.fDialogService.warn("getList", ret.msg);
+  }
+
+  async refreshData(): Promise<void> {
+    await this.getList();
+  }
+  async addEdi(): Promise<void> {
+    await this.router.navigate([`/${FConstants.EDI_REQUEST_URL}`]);
+  }
+  async open(data: EDIUploadModel): Promise<void> {
+    await this.router.navigate([`/${FConstants.EDI_LIST_URL}/${data.thisPK}`]);
+  }
+  async startDateChange(data: any): Promise<void> {
+    if (this.startDate.getTime() > this.endDate.getTime()) {
+      const buff = new Date(this.startDate);
+      this.startDate = new Date(this.endDate);
+      this.endDate = new Date(buff);
+    }
+
+    const diffDay = FExtensions.calcDateDiffDay(this.startDate, this.endDate);
+    if (diffDay > 366) {
+      this.endDate.setTime(this.startDate.getTime() + 365 * 1000 * 24 * 60 * 60);
+      this.endCalendar.updateInputfield();
+    }
+  }
+  async endDateChange(data: any): Promise<void> {
+    if (this.startDate.getTime() > this.endDate.getTime()) {
+      const buff = new Date(this.startDate);
+      this.startDate = new Date(this.endDate);
+      this.endDate = new Date(buff);
+    }
+
+    const diffDay = FExtensions.calcDateDiffDay(this.startDate, this.endDate);
+    if (diffDay > 366) {
+      this.startDate.setTime(this.endDate.getTime() - 365 * 1000 * 24 * 60 * 60);
+      this.startCalendar.updateInputfield();
+    }
+  }
+
+  get filterFields(): string[] {
+    return ["orgName"];
+  }
+  get startDateTooltip(): string {
+    return "edi-list.header.start-date";
+  }
+  get endDateTooltip(): string {
+    return "edi-list.header.end-date";
+  }
+  getApplyDate(data: EDIUploadModel): string {
+    return `${data.year}-${data.month}`;
+  }
+
+  protected readonly customSort = FExtensions.customSort;
+  protected readonly filterTable = FExtensions.filterTable;
+  protected readonly dateToYYYYMMdd = FExtensions.dateToYYYYMMdd;
+  protected readonly getEdiStateSeverity = getEdiStateSeverity;
+  protected readonly EDIStateToEDIStateDesc = EDIStateToEDIStateDesc;
+  protected readonly tableStyle = FConstants.tableStyle;
+  protected readonly filterTableOption = FConstants.filterTableOption;
 }
--- src/app/components/app-main/edi/edi-list/edi-list.module.ts
+++ src/app/components/app-main/edi/edi-list/edi-list.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import {EdiListComponent} from "./edi-list.component";
 import {EdiListRoutingModule} from "./edi-list-routing.module";
+import {Button} from "primeng/button";
+import {IconField} from "primeng/iconfield";
+import {InputIcon} from "primeng/inputicon";
+import {InputText} from "primeng/inputtext";
+import {TableModule} from "primeng/table";
+import {Tag} from "primeng/tag";
+import {TranslatePipe} from "@ngx-translate/core";
+import {DatePicker} from "primeng/datepicker";
+import {FormsModule} from "@angular/forms";
+import {Tooltip} from "primeng/tooltip";
 
 
 
 @NgModule({
   declarations: [EdiListComponent],
   imports: [
-    CommonModule, EdiListRoutingModule
+    CommonModule, EdiListRoutingModule, Button, IconField, InputIcon, InputText, TableModule, Tag, TranslatePipe, DatePicker, FormsModule, Tooltip
   ]
 })
 export class EdiListModule { }
