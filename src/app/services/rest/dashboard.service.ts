f57697c53bc93b1352d096d5655e599d113dc875
HaMyoungHoon <basicouts@gmail.com>
2025-01-03 09:08:18 +0900

    dashboard service 추가

---
+++ src/app/services/rest/dashboard.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {HttpResponseInterceptorService} from "../common/http-response-interceptor.service";
+import {RestResult} from "../../models/common/rest-result";
+import {HowMuchModel} from "../../models/rest/how-much-model";
+import {HowMuchHospitalModel} from "../../models/rest/how-much-hospital-model";
+import {HowMuchPharmaModel} from "../../models/rest/how-much-pharma-model";
+import {HowMuchMedicineModel} from "../../models/rest/how-much-medicine-model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private baseUrl = "/apiCSO/extra/dashboard";
+
+  constructor(private httpResponse: HttpResponseInterceptorService) { }
+
+  getList(date: string): Promise<RestResult<HowMuchModel[]>> {
+    this.httpResponse.addParam("date", date);
+    return this.httpResponse.get(`${this.baseUrl}/list`);
+  }
+  getListHos(date: string): Promise<RestResult<HowMuchHospitalModel[]>> {
+    this.httpResponse.addParam("date", date);
+    return this.httpResponse.get(`${this.baseUrl}/list/hos`);
+  }
+  getListPharma(date: string): Promise<RestResult<HowMuchPharmaModel[]>> {
+    this.httpResponse.addParam("date", date);
+    return this.httpResponse.get(`${this.baseUrl}/list/pharma`);
+  }
+  getListMedicine(date: string): Promise<RestResult<HowMuchMedicineModel[]>> {
+    this.httpResponse.addParam("date", date);
+    return this.httpResponse.get(`${this.baseUrl}/list/medicine`);
+  }
+}
