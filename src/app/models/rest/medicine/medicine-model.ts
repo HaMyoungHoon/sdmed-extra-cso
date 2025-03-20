66724f71d13fd4762cfcf86de6f50654a368d689
하명훈 <basicouts@gmail.com>
2025-03-20 20:06:58 +0900

    약품 데이터베이스 변경

--- src/app/models/rest/medicine/medicine-model.ts
+++ src/app/models/rest/medicine/medicine-model.ts
@@ -1,62 +1,65 @@
 import {MedicinePriceModel} from "./medicine-price-model";
-import {MedicineSubModel} from "./medicine-sub-model";
 import {MedicineIngredientModel} from "./medicine-ingredient-model";
+import {MedicineDiv} from "./medicine-div";
 
 export class MedicineModel {
   thisPK: string = "";
-  code: string = "";
-  mainIngredientCode: string = "";
-  kdCode: string = "";
-  standardCode: number = 0;
-  clientName?: string = "";
   makerName?: string = "";
-  makerCode: string = "";
-  orgName: string = ""
+  orgName: string = "";
   innerName: string = "";
+  kdCode: string = "";
   customPrice: number = 0;
   charge: number = 0;
-  maxPrice: number = 0;
+  standard: string = "";
+  etc1: string = "";
+  mainIngredientCode: string = "";
+  code: string = "";
+  makerCode: string = "";
+  medicineDiv: MedicineDiv = MedicineDiv.Open;
   inVisible: boolean = false;
-  medicineSubModel: MedicineSubModel = new MedicineSubModel();
+  clientName?: string = "";
+  maxPrice: number = 0;
   medicineIngredientModel: MedicineIngredientModel = new MedicineIngredientModel();
   medicinePriceModel: MedicinePriceModel[] = [];
 
   init(data: MedicineModel): MedicineModel {
     this.thisPK = data.thisPK;
-    this.code = data.code;
-    this.mainIngredientCode = data.mainIngredientCode;
-    this.kdCode = data.kdCode;
-    this.standardCode = data.standardCode;
-    this.clientName = data.clientName ?? "";
-    this.makerName = data.makerName ?? "";
-    this.makerCode = data.makerCode;
+    this.makerName = data.makerName;
     this.orgName = data.orgName;
     this.innerName = data.innerName;
+    this.kdCode = data.kdCode;
     this.customPrice = data.customPrice;
     this.charge = data.charge;
-    this.maxPrice = data.maxPrice;
+    this.standard = data.standard;
+    this.etc1 = data.etc1;
+    this.mainIngredientCode = data.mainIngredientCode;
+    this.code = data.code;
+    this.makerCode = data.makerCode;
+    this.medicineDiv = data.medicineDiv;
     this.inVisible = data.inVisible;
-    this.medicineSubModel = data.medicineSubModel;
+    this.clientName = data.clientName;
+    this.maxPrice = data.maxPrice;
     this.medicineIngredientModel = data.medicineIngredientModel;
     this.medicinePriceModel = data.medicinePriceModel;
     return this;
   }
   copyLhsFromRhs(lhs: MedicineModel, rhs: MedicineModel): void {
     lhs.thisPK = rhs.thisPK;
-    lhs.code = rhs.code;
-    lhs.mainIngredientCode = rhs.mainIngredientCode;
-    lhs.kdCode = rhs.kdCode;
-    lhs.standardCode = rhs.standardCode;
-    lhs.clientName = rhs.clientName ?? "";
-    lhs.makerName = rhs.makerName ?? "";
-    lhs.makerCode = rhs.makerCode;
+    lhs.makerName = rhs.makerName;
     lhs.orgName = rhs.orgName;
     lhs.innerName = rhs.innerName;
+    lhs.kdCode = rhs.kdCode;
     lhs.customPrice = rhs.customPrice;
     lhs.charge = rhs.charge;
-    lhs.maxPrice = rhs.maxPrice;
+    lhs.standard = rhs.standard;
+    lhs.etc1 = rhs.etc1;
+    lhs.mainIngredientCode = rhs.mainIngredientCode;
+    lhs.code = rhs.code;
+    lhs.makerCode = rhs.makerCode;
+    lhs.medicineDiv = rhs.medicineDiv;
     lhs.inVisible = rhs.inVisible;
-    lhs.medicineSubModel = rhs.medicineSubModel;
+    lhs.clientName = rhs.clientName;
+    lhs.maxPrice = rhs.maxPrice;
     lhs.medicineIngredientModel = rhs.medicineIngredientModel;
     lhs.medicinePriceModel = rhs.medicinePriceModel;
   }
